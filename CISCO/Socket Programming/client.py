0d545740eeda8b3fa6abf7fa1c108296019d1e90
AMEY THAKUR <ameythakur20@gmail.com>
2021-07-30 14:08:46 +0530

    CN

---
+++ CISCO/Socket Programming/client.py
@@ -0,0 +1,9 @@
+import socket
+
+c = socket.socket()
+
+c.connect(('localhost',9999))
+name  = input("Enter your Name:")
+c.send(bytes(name,'utf-8'))
+
+print(c.recv(1024).decode())
---
+++ CISCO/Socket Programming/server.py
@@ -0,0 +1,17 @@
+import socket
+
+s = socket.socket()
+print('Socket Created')
+
+s.bind(('localhost', 9999))
+
+s.listen(3)
+print('Waiting for the connection')
+
+while True:
+    c , addr = s.accept()
+
+    name = c.recv(1024).decode()
+    print('Connected with : ', addr , name)
+    c.send(bytes('Thankyou','utf-8'))
+    c.close()
